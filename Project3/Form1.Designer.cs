c03415f20c1e23cd78979b93360cbef5124edfdb
Anas Alhalabi <you@example.com>
2024-01-09 11:16:23 -0800

    Add Project

---
+++ Project3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Project3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.guessTextBox = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Location = new System.Drawing.Point(33, 23);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(35, 13);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "label1";
+            this.promptLabel.Click += new System.EventHandler(this.promptLabel_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(42, 186);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "label2";
+            // 
+            // guessTextBox
+            // 
+            this.guessTextBox.Location = new System.Drawing.Point(36, 53);
+            this.guessTextBox.Name = "guessTextBox";
+            this.guessTextBox.Size = new System.Drawing.Size(113, 20);
+            this.guessTextBox.TabIndex = 2;
+            this.guessTextBox.TextChanged += new System.EventHandler(this.guessTextBox_TextChanged);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(36, 99);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(91, 23);
+            this.b1.TabIndex = 3;
+            this.b1.Text = "Try the Number";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(136, 181);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 23);
+            this.b2.TabIndex = 5;
+            this.b2.Text = "Play Again";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.IndianRed;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 212);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(255, 197);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 422);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.guessTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.promptLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox guessTextBox;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Project3/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void promptLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int secretNumber;
+        private bool gameActive;
+
+
+
+        private void InitializeGame()
+        {
+            Random random = new Random();
+            secretNumber = random.Next(1, 101);
+            gameActive = true;
+
+            promptLabel.Text = "I have a number between 1 and 100 -- can you guess my number?";
+            resultLabel.Text = "";
+            guessTextBox.Text = "";
+            guessTextBox.Enabled = true;
+            b1.Enabled = true;
+            this.BackColor = DefaultBackColor;
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (!gameActive)
+            {
+                MessageBox.Show("Please click 'Play Again' to start a new game.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                int userGuess = int.Parse(guessTextBox.Text);
+                CheckGuess(userGuess);
+                guessTextBox.Text = ""; 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void CheckGuess(int userGuess)
+        {
+            int difference = Math.Abs(secretNumber - userGuess);
+
+            if (difference == 0)
+            {
+                MessageBox.Show("Congratulations! You guessed the correct number.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.BackColor = DefaultBackColor;
+                gameActive = false;
+            }
+            else
+            {   
+                this.BackColor = difference > 25 ? Color.Red : Color.Blue;
+                resultLabel.Text = difference > 25 ? "Warmer" : "Colder";
+
+            }
+        }
+
+        
+
+        private void guessTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b2_Click_1(object sender, EventArgs e)
+        {
+            this.BackColor = DefaultBackColor;
+            guessTextBox.Text = "";
+            InitializeGame();
+        }
+    }
+
+}
